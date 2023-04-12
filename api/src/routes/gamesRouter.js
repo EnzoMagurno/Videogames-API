bd160abffb760668e2a5f9878023ce94864d5c7d
EnzoMagurno <magurnoenzo31602@gmail.com>
2023-04-12 15:29:48 -0300

    module into gamesRouter

--- api/src/routes/gamesRouter.js
+++ api/src/routes/gamesRouter.js
@@ -1,24 +1,9 @@
 const { Router } = require('express');
 const gamesRouter = Router();
-
 const getGames = require('../controllers/getGames');
 const getGameById = require('../controllers/getGameById');
 
-gamesRouter.get('/', async (req, res) => {
-  try {
-    const data = await getGames();
-    res.status(200).json(data);
-  } catch (error) {
-    res.status(400).json(error.message);
-  }
-});
+gamesRouter.get('/', getGames);
 
-gamesRouter.get('/:id', async (req, res) => {
-  try {
-    const data = await getGameById(req,res);
-    res.status(200).json(data);
-  } catch (error) {
-    res.status(400).json(error.message);
-  }
-});
+gamesRouter.get('/:id', );
 module.exports = gamesRouter;
