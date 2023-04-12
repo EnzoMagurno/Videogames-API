a72e302fe03309943015c251b7728839f1a5eb36
EnzoMagurno <magurnoenzo31602@gmail.com>
2023-04-12 15:11:50 -0300

    clean code into getGameById

--- api/src/controllers/getGameById.js
+++ api/src/controllers/getGameById.js
@@ -4,13 +4,11 @@ require('dotenv').config();
 
 const getGameById = async (req, res) => {
   const { id } = req.params;
-  const response = await axios.get(`${API}/games/${id}?key=${API_KEY}`);
-  const data = response.data;
-  return data;
+  return (await axios.get(`${API}/games/${id}?key=${API_KEY}`)).data;
 };
 
+//   const response = await axios.get(`${API}/games/${id}?key=${API_KEY}`);
+//   const data = response.data;
+//   return data;
+//    return await (axios.get(`${API}/games/${id}?key=${API_KEY}`)).data;
 module.exports = getGameById;
-  //   const response = await axios.get(`${API}/games/${id}?key=${API_KEY}`);
-  //   const data = response.data;
-  //   return data;
-  //    return await (axios.get(`${API}/games/${id}?key=${API_KEY}`)).data;
--- api/src/routes/gamesRouter.js
+++ api/src/routes/gamesRouter.js
@@ -1,7 +1,9 @@
 const { Router } = require('express');
 const gamesRouter = Router();
+
 const getGames = require('../controllers/getGames');
 const getGameById = require('../controllers/getGameById');
+
 gamesRouter.get('/', async (req, res) => {
   try {
     const data = await getGames();
